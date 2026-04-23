fa9b05d0ed6060203b98b50b65f4804d69d7c3df
ULb111 <ila.dem69@gmail.com>
2026-04-23 17:28:29 +0300

    Update script.py
    добавлен зажим и меню как и приказали)

--- script.py
+++ script.py
@@ -1,9 +1,32 @@
+import time #y=запуск f=остановка g=кнопка которую жмут
 import keyboard
-print("нажми / чтобы запустить кликер, esc чтобы остановить")
-keyboard.wait("/")
-while True:
-    keyboard.send("w")
+print("<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+print("выберите пункт:")
+print("1.кликер")
+print("2.зажатие клавиш")
+print("3.выход")
+print("<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+x = int(input())
+if x == 1:
+    y = input("выберите клавишу запуска:")
+    f = input("выбеите клавишу остановки:")
+    g = input("выберете клавишу клика:")
+    keyboard.wait(y)
+    while True:
+        time.sleep(0.2)
+        keyboard.send(g)
+        if keyboard.is_pressed(f):
+            break
+    print("остановка")
 
-    if keyboard.is_pressed("esc"):
-        print("остановка")
-        break
+elif x == 2:
+    y = input("выберите клавишу запуска:")
+    f = input("выбеите клавишу остановки:")
+    g = input("выберете клавишу зажатия:")
+    keyboard.wait(y)
+    keyboard.press(g)
+    if keyboard.is_pressed(f):
+        keyboard.release(g)
+    print("остановка")
+elif x == 3:
+    exit()
